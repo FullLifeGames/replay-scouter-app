736c8d5337490ac70c2f5e21f1da395b1f5bbfce
FullLifeGames <FullLifeGames@users.noreply.github.com>
2023-03-11 18:02:43 +0100

    Filter for text representation

--- src/components/Representation/TextRepresentation.vue
+++ src/components/Representation/TextRepresentation.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <SearchQuery :scouting-result="props.scoutingResult" @change="change" />
     <b-form-textarea
       v-if="scoutingResultRaw !== ''"
       v-model="scoutingResultRaw"
@@ -9,22 +10,32 @@
 </template>
 
 <script setup lang="ts">
-import type { ApiScoutingResult } from "@/api";
+import type { ApiScoutingResult, Team } from "@/api";
 
 const props = defineProps<{
   scoutingResult: ApiScoutingResult | null;
 }>();
 
+const teams = ref(props.scoutingResult?.teams ?? ([] as Team[]));
+const outputTeams = ref(
+  props.scoutingResult?.outputs?.teams ?? ([] as string[])
+);
+
+const change = (searchedTeams: Team[], searchedOutputs: string[]) => {
+  teams.value = searchedTeams;
+  outputTeams.value = searchedOutputs;
+};
+
 const scoutingResultRaw = computed(() => {
   if (
     props.scoutingResult &&
     props.scoutingResult.outputs &&
-    props.scoutingResult.outputs.teams
+    outputTeams.value
   ) {
     return (
       props.scoutingResult.outputs.header +
       "\n\n" +
-      props.scoutingResult.outputs.teams.join("\n\n\n")
+      outputTeams.value.join("\n\n\n")
     );
   }
   return "";
