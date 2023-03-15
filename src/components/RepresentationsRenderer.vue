995b36b8d44650ba18d0c15d36e37ad7dcd4a45b
Alex <alexandercoding404@gmail.com>
2023-03-15 22:51:05 +0100

    Paste (Bugs)

--- src/components/RepresentationsRenderer.vue
+++ src/components/RepresentationsRenderer.vue
@@ -74,6 +74,12 @@
         :teams="teams"
         :output-teams="outputTeams"
       />
+      <PasteRepresentation
+        v-if="selectedRepresentation === 'PasteRepresentation'"
+        :scouting-result="props.scoutingResult"
+        :teams="teams"
+        :output-teams="outputTeams"
+      />
     </div>
   </div>
 </template>
@@ -118,6 +124,7 @@ const representations = [
   { text: "Raw Representation", value: "RawRepresentation" },
   { text: "Text Representation", value: "TextRepresentation" },
   { text: "Table Representation", value: "TableRepresentation" },
+  { text: "Paste Representation", value: "PasteRepresentation" },
   defaultRepresentation,
   { text: "Pokémon Statistics (Table)", value: "PokemonStatistics" },
   { text: "Combos Statistics (Table)", value: "CombosStatisticsWithoutLeads" },
